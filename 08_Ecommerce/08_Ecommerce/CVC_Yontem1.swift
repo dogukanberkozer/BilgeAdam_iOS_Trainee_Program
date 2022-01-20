a81acef558b1f8e54dddff6cb769681cc2f0d714
Dogukan Berk OZER <38347138+dogukanberkozer@users.noreply.github.com>
2022-01-20 11:50:40 +0300

    Add files via upload

---
+++ 08_Ecommerce/08_Ecommerce/CVC_Yontem1.swift
@@ -0,0 +1,20 @@
+//
+//  CVC_Yontem1.swift
+//  08_Ecommerce
+//
+//  Created by Dogukan Berk Ozer on 19.01.2022.
+//
+
+import UIKit
+
+
+
+class CVC_Yontem1: UICollectionViewCell {
+    
+
+    @IBOutlet weak var imgCvc: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblOldPrice: UILabel!
+    @IBOutlet weak var lblPrice: UILabel!
+    
+}
---
+++ 08_Ecommerce/08_Ecommerce/CVC_Yontem2.swift
@@ -0,0 +1,17 @@
+//
+//  CVC_Yontem2.swift
+//  08_Ecommerce
+//
+//  Created by Dogukan Berk Ozer on 19.01.2022.
+//
+
+import UIKit
+
+class CVC_Yontem2: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
